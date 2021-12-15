1f5f3312c321d07837c1a3a27d815d33279fc1e9
sjransom <sjransom@gmail.com>
2021-12-15 10:57:18 +0000

    refactor fetching jokes into own component

--- App.tsx
+++ App.tsx
@@ -1,54 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import {
-  ActivityIndicator,
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native'
+import React from 'react'
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
 
-type Data = {
-  jokes: [string]
-}
+import FetchData from './components/FetchData'
 
 const App: React.FC = () => {
-  const [isLoading, setLoading] = useState<boolean>(true)
-  const [data, setData] = useState<Data>()
-
-  const getMovies = async () => {
-    try {
-      const response = await fetch(
-        'https://z2aeyohis1.execute-api.eu-west-1.amazonaws.com/default/jokes'
-      )
-      const jokes = await response.json()
-      setData(jokes)
-    } catch (error) {
-      console.error(error)
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    getMovies()
-  }, [])
-
-  console.log(data)
-
   return (
     <SafeAreaView>
       <ScrollView contentContainerStyle={styles.container}>
         <View>
           <Text>Bad Jokes</Text>
         </View>
-        <View>
-          {isLoading ? (
-            <ActivityIndicator size="large" color="#0000ff" />
-          ) : (
-            <Text>{data?.jokes[0]}</Text>
-          )}
-        </View>
+        <FetchData />
       </ScrollView>
     </SafeAreaView>
   )
---
+++ components/FetchData.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+
+import { ActivityIndicator, Text, View } from 'react-native'
+
+type Data = {
+  jokes: [string]
+}
+
+const Fetch: React.FC = () => {
+  const [isLoading, setLoading] = useState<boolean>(true)
+  const [data, setData] = useState<Data>()
+
+  const getMovies = async () => {
+    try {
+      const response = await fetch(
+        'https://z2aeyohis1.execute-api.eu-west-1.amazonaws.com/default/jokes'
+      )
+      const jokes = await response.json()
+      setData(jokes)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getMovies()
+  }, [])
+
+  console.log(data)
+
+  return (
+    <View>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <Text>{data?.jokes[0]}</Text>
+      )}
+    </View>
+  )
+}
+
+export default Fetch
