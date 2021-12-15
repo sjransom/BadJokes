cbb45cb4044056327a7a9da1d13271d2e2a32699
sjransom <sjransom@gmail.com>
2021-12-15 13:30:28 +0000

    save data to storage

--- components/FetchData.tsx
+++ components/FetchData.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
-
 import { ActivityIndicator, Text, View } from 'react-native'
+
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 type Data = {
   jokes: [string]
@@ -10,7 +11,7 @@ const Fetch: React.FC = () => {
   const [isLoading, setLoading] = useState<boolean>(true)
   const [data, setData] = useState<Data>()
 
-  const getMovies = async () => {
+  const getJokes = async () => {
     try {
       const response = await fetch(
         'https://z2aeyohis1.execute-api.eu-west-1.amazonaws.com/default/jokes'
@@ -24,18 +25,40 @@ const Fetch: React.FC = () => {
     }
   }
 
-  useEffect(() => {
-    getMovies()
-  }, [])
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('@storage_Key')
+      return jsonValue != null ? JSON.parse(jsonValue) : null
+    } catch (e) {
+      // error reading value
+    }
+  }
 
-  console.log(data)
+  const storeData = async (value: any) => {
+    try {
+      const jsonValue = JSON.stringify(value)
+      await AsyncStorage.setItem('@storage_Key', jsonValue)
+    } catch (e) {
+      // saving error
+    }
+  }
+
+  storeData(data && data.jokes)
+
+  useEffect(() => {
+    async function getSavedJokes() {
+      await getData()
+    }
+    getSavedJokes()
+    getJokes()
+  }, [])
 
   return (
     <View>
       {isLoading ? (
         <ActivityIndicator size="large" color="#0000ff" />
       ) : (
-        <Text>{data?.jokes[0]}</Text>
+        <Text>{data && data.jokes ? data.jokes[0] : 'No jokes :('}</Text>
       )}
     </View>
   )
